246180bffa72453b8ed6ee6facd22bc7cc8db7ee
Allan Muturi <amuturi01@gmail.com>
2021-06-13 22:23:07 +0300

    Add loading animation

---
+++ frontend/src/components/LoaderAnim.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactLoading from 'react-loading'
+
+function LoaderAnim() {
+    return (
+        <ReactLoading type='spin' color='blue' height='20%' width='20%'/>
+    )
+}
+
+export default LoaderAnim
